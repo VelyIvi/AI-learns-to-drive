3a1f4273e3ec2f0ced90011f3701c1e378bcd2c7
Ivi_ <ivan.velychko.me@gmail.com>
2022-09-26 12:05:25 +0200

    Saving of map Done

--- map.h
+++ map.h
@@ -2,13 +2,14 @@
 #include <fstream>
 #include <string>
 
+#include <ctime>
 
 using json = nlohmann::json;
 
 
 struct Map{
     private:
-        std::string name = "no";
+        std::string name = "Untitled";
     public:
         std::vector<std::vector<Vector2>>* map_points = new std::vector<std::vector<Vector2>>;
         Map();
@@ -28,22 +29,23 @@ Map::~Map(){
 
 void Map::Save_To_Json(){
     json j;
-    int arrayXSize = 0;
-    for(int x = 0; x < map_points->size(); x++){
-        if (map_points->at(x).size() > arrayXSize){
-            arrayXSize = map_points->at(x).size();
-        }
-    }
-    float pointsee[map_points->size()][arrayXSize][2];
 
-    for(int a = 0; a < map_points->size(); a++){
-        for(int b = 0; b < map_points->at(a).size(); b++){
-            pointsee[a][b][0] = map_points->at(a).at(b).x;
-            pointsee[a][b][1] = map_points->at(a).at(b).y;
+    time_t now = time(0);
+    char* dt = ctime(&now);
+
+
+    auto result = json{
+        {"item", json::array()},
+    };
+
+    for (int i = 0; i < map_points->size(); i++) {
+        auto& outer = result["item"];
+        for (int j = 0; j < map_points->at(i).size(); j++) {
+            // You'll need to convert whatever Items is into a JSON type first
+            outer[i].push_back({map_points->at(i).at(j).x, map_points->at(i).at(j).y});
         }
     }
-    std::cout<<pointsee<<"\n";
-    // j["wall"] = pointsee;
-    std::ofstream file(name+".json");
-    file << j;
+
+    std::ofstream file(name + dt + ".json");
+    file << result;
 }
